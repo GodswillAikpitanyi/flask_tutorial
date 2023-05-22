8ec17cbf50e013d8b1d22fe8cacba5ea91e380c4
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-05-22 07:53:12 +0000

    flask tutorial

---
+++ app.py
@@ -0,0 +1,18 @@
+#python3
+'new page'
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def rootpage():
+    'welcome root page'
+
+    name = ''
+    if request.method == 'POST' and 'username' in request.form:
+        name = request.form.get('username')
+    return render_template("index.html",
+                         name=name)
+
+app.run()
